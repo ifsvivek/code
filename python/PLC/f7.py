52d7b9c1c2e52e6fbe46d5f4dd21d5acf3d58114
Vivek Sharma <vs63961@gmail.com>
2023-05-11 13:31:23 +0530

    update

--- python/PLC/f7.py
+++ python/PLC/f7.py
@@ -1,16 +1,6 @@
 n=int(input('Enter a number: '))
-
 for i in range(2,int(n**0.5)+1):
-
     if n%i==0:
-
         print(n,'is not a prime number')
-
     else:
-
         print(n,'is a prime number')
-
-
-
-
-
