e3d828d65d86bafe0d777a2745f8c163381998d8
Vivek <vs63961@gmail.com>
2023-06-10 19:04:47 +0530

    removed

--- C/module/q11.c
+++ C/module/q11.c
@@ -7,44 +7,33 @@ an array of N real numbers.*/
 void computeStatistics(const float *arr, int size, float *sum, float *mean, float *stdDev)
 {
     *sum = *mean = *stdDev = 0.0;
-
     for (int i = 0; i < size; i++)
     {
         *sum += arr[i];
     }
-
     *mean = *sum / size;
-
     for (int i = 0; i < size; i++)
     {
         float deviation = arr[i] - *mean;
         *stdDev += deviation * deviation;
     }
-
     *stdDev = sqrt(*stdDev / size);
 }
-
 int main()
 {
     int N;
+    float arr[N], sum, mean, stdDev;
 
     printf("Enter the number of elements: ");
     scanf("%d", &N);
-
-    float arr[N];
-
     printf("Enter the elements:\n");
     for (int i = 0; i < N; i++)
     {
         scanf("%f", &arr[i]);
     }
-
-    float sum, mean, stdDev;
     computeStatistics(arr, N, &sum, &mean, &stdDev);
-
     printf("Sum: %.2f\n", sum);
     printf("Mean: %.2f\n", mean);
     printf("Standard Deviation: %.2f\n", stdDev);
-
     return 0;
 }
