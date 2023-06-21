cee74917b61d58e1f109062568f46d602a6901f5
Vivek <vs63961@gmail.com>
2023-06-21 19:41:26 +0530

    q1

--- C/module/q1.c
+++ C/module/q1.c
@@ -1,42 +1,25 @@
 // Simple Calculator
 #include <stdio.h>
 
-int main()
-{
-    int num1, num2;
+int main() {
+    double num1, num2;
     char operator;
-
+    
     printf("Enter the first number: ");
-    scanf("%d", &num1);
-
+    scanf("%lf", &num1);
+    
     printf("Enter an operator (+, -, *, /): ");
     scanf(" %c", &operator);
-
+    
     printf("Enter the second number: ");
-    scanf("%d", &num2);
-
-    if (operator== '+')
-    {
-        printf("%d + %d = %d\n", num1, num2, num1 + num2);
-    }
-    else if (operator== '-')
-    {
-        printf("%d - %d = %d\n", num1, num2, num1 - num2);
-    }
-    else if (operator== '*')
-    {
-        printf("%d * %d = %d\n", num1, num2, num1 * num2);
-    }
-    else if (operator== '/')
-    {
-        if (num2 != 0)
-            printf("%d / %d = %.2f\n", num1, num2, (float)num1 / num2);
-        else
-            printf("Error: Division by zero is not allowed.\n");
-    }
-    else
-    {
-        printf("Error: Invalid operator.\n");
-    }
+    scanf("%lf", &num2);
+    
+    double result = (operator == '+') ? num1 + num2 : 
+                    (operator == '-') ? num1 - num2 : 
+                    (operator == '*') ? num1 * num2 : 
+                    (operator == '/') ? num1 / num2 : 0.0;
+    
+    printf("Result: %lf\n", result);
+    
     return 0;
 }
