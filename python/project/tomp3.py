912fea6bc74bc293b63e19418493a77094d0ded1
ifsvivek <vs63961@gmai.com>
2023-07-19 22:42:06 +0530

    to mp3

--- python/project/tomp3.py
+++ python/project/tomp3.py
@@ -1,13 +1,15 @@
 import ffmpeg
 
-def convert_mp4_to_mp3(input_file, output_file):
-    input_stream = ffmpeg.input(input_file)
+def convert_and_trim_mp4_to_mp3(input_file, output_file, start_time, end_time):
+    input_stream = ffmpeg.input(input_file, ss=start_time, t=end_time - start_time)
     audio_stream = input_stream.audio
     output_stream = ffmpeg.output(audio_stream, output_file, codec='libmp3lame', qscale=0)
     ffmpeg.run(output_stream)
 
 if __name__ == "__main__":
-    input_file_path = "in.mp4"
-    output_file_path = "out.mp3"
+    input_file_path = "input_video.mp4"
+    output_file_path = "output_audio.mp3"
+    start_time_seconds = 0
+    end_time_seconds = 180
 
-    convert_mp4_to_mp3(input_file_path, output_file_path)
+    convert_and_trim_mp4_to_mp3(input_file_path, output_file_path, start_time_seconds, end_time_seconds)
