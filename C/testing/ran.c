76f8f70a2a9bbd5fc0254d4b7883641bd36208cc
ifsvivek <vs63961@gmai.com>
2023-07-21 10:53:43 +0530

    fix

--- C/testing/ran.c
+++ C/testing/ran.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main() {
-    int cash = 233;
+    int cash = 366;
 
     if (cash / 100 >= 1) {
         printf("100x%d", cash / 100);
         cash %= 100;
     }
-    else if (cash / 50 >= 1) {
+    if (cash / 50 >= 1) {
         printf("\n50x%d", cash / 50);
         cash %= 50;
     }
-    else if (cash / 10 >= 1) {
+    if (cash / 10 >= 1) {
         printf("\n10x%d", cash / 10);
         cash %= 10;
     }
