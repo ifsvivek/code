a0c595dfc6fde9f0bee316b5cd56bcf1c8fbc1d9
Vivek Sharma <80456739+ifsvivek@users.noreply.github.com>
2023-07-21 10:49:09 +0530

    Update ran.c

--- C/testing/ran.c
+++ C/testing/ran.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
-int main(){
-    int cash=233;
-    if (cash/100)>=1{
-        printf("100x%d",cash%100);
-        }
-    else if (cash/50)>=1{
-        printf("\n50x%d",cash%50);
+int main() {
+    int cash = 233;
+
+    if (cash / 100 >= 1) {
+        printf("100x%d", cash / 100);
+        cash %= 100;
     }
-    else (cash/10)>=1{
-        printf("\n10x%",cash%50);
+    else if (cash / 50 >= 1) {
+        printf("\n50x%d", cash / 50);
+        cash %= 50;
     }
-    printf("\n1x%d",cash%50);
+    else if (cash / 10 >= 1) {
+        printf("\n10x%d", cash / 10);
+        cash %= 10;
+    }
+    printf("\n1x%d", cash);
 }
