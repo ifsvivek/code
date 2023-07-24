7f47f5840f2e10e37bf113bf3d529fd5e9a08a40
ifsvivek <vs63961@gmai.com>
2023-07-24 18:39:05 +0530

    fix

--- C/testing/ran1.c
+++ C/testing/ran1.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
-int main (){
-    float time=2.3;
-    int hour;
-    while (time>=1)
-    {
+#include <stdio.h>
+
+int main() {
+    float time = 2.3;
+    int hour = 0;
+    while (time >= 1) {
         time--;
-        hour++;/* code */
+        hour++;
     }
-    printf("%d:%0.1f",hour,time);
+    printf("%d:%.0f\n", hour, time * 60);
+    return 0;
 }
