923b5995368053c4e93b026785aa772193334d65
ifsvivek <vs63961@gmai.com>
2023-07-27 20:43:56 +0530

    idk what happend here

--- C/module/q1.c
+++ C/module/q1.c
@@ -1,19 +1,38 @@
-// Simple Calculator
 #include <stdio.h>
 
 int main() {
-    double num1, num2;
     char operator;
-    
-    printf("Enter the Equation: ");
-    scanf("%lf%c%lf",&num1,&operator,&num2);
-    
-    double result = (operator == '+') ? num1 + num2 : 
-                    (operator == '-') ? num1 - num2 : 
-                    (operator == '*') ? num1 * num2 : 
-                    (operator == '/') ? num1 / num2 : 0.0;
-    
-    printf("Result: %.2f\n", result);
-    
+    float num1, num2, result = 0;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                printf("Error: Division by zero is not allowed.\n");
+                return 1;
+            }
+            break;
+        default:
+            printf("Error: Invalid operator.\n");
+            return 1;
+    }
+
+    printf("%0.2f %c %0.2f = %.2f\n", num1, operator, num2, result);
     return 0;
 }
