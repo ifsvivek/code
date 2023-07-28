a00c595fd56dcbe52483a86495f052a8c577488f
ifsvivek <vs63961@gmai.com>
2023-07-28 16:05:26 +0530

    fix

--- C/module/q6.c
+++ C/module/q6.c
@@ -4,7 +4,7 @@
 
 #define MAX_SIZE 10
 
-void matrixMultiplication(int m1[][MAX_SIZE], int r1, int c1, int m2[][MAX_SIZE], int r2, int c2, int res[][MAX_SIZE]){
+void mul(int m1[][MAX_SIZE], int r1, int c1, int m2[][MAX_SIZE], int r2, int c2, int res[][MAX_SIZE]){
     if (c1 != r2){
         printf("Error: Invalid matrix dimensions for multiplication.\n");
         return;
@@ -19,7 +19,7 @@ void matrixMultiplication(int m1[][MAX_SIZE], int r1, int c1, int m2[][MAX_SIZE]
     }
 }
 
-void displayMatrix(int matrix[][MAX_SIZE], int rows, int cols){
+void display(int matrix[][MAX_SIZE], int rows, int cols){
     for (int i = 0; i < rows; i++){
         for (int j = 0; j < cols; j++){
             printf("%d\t", matrix[i][j]);
@@ -47,14 +47,14 @@ int main(){
             scanf("%d", &m2[i][j]);
 
     int res[MAX_SIZE][MAX_SIZE];
-    matrixMultiplication(m1, r1, c1, m2, r2, c2, res);
+    mul(m1, r1, c1, m2, r2, c2, res);
 
     printf("\nMatrix 1:\n");
-    displayMatrix(m1, r1, c1);
+    display(m1, r1, c1);
     printf("Matrix 2:\n");
-    displayMatrix(m2, r2, c2);
+    display(m2, r2, c2);
     printf("Resultant Matrix:\n");
-    displayMatrix(res, r1, c2);
+    display(res, r1, c2);
 
     return 0;
 }
