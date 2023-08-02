ce8a3bf74c18f67388cd1b8c13dd1a058c15ff3d
ifsvivek <vs63961@gmai.com>
2023-08-02 19:46:24 +0530

    Assignment-2

---
+++ C/Assignment-2/q1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int N;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &N);
+
+    int A[N], sumOdd = 0, sumEven = 0, totalSum = 0, oddCount = 0, evenCount = 0;
+
+    printf("Enter %d integers:\n", N);
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &A[i]);
+        totalSum += A[i];
+
+        if (A[i] % 2 == 0)
+        {
+            sumEven += A[i];
+            evenCount++;
+        }
+        else
+        {
+            sumOdd += A[i];
+            oddCount++;
+        }
+    }
+
+    float average = (float)totalSum / N;
+
+    printf("Sum of odd numbers: %d\nSum of even numbers: %d\nAverage of all numbers: %.2f\n", sumOdd, sumEven, average);
+
+    return 0;
+}
---
+++ C/Assignment-2/q2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    char input[100], output[100];
+
+    printf("Enter a string: ");
+    fgets(input, sizeof(input), stdin);
+
+    int i = 0;
+    while (input[i] != '\0')
+    {
+        char c = input[i];
+
+        if ((c >= 'a' && c < 'z') || (c >= 'A' && c < 'Z'))
+        {
+            output[i] = c + 1;
+        }
+        else if (c == 'z' || c == 'Z')
+        {
+            output[i] = c - 25;
+        }
+        else
+        {
+            output[i] = c;
+        }
+
+        i++;
+    }
+
+    output[i] = '\0';
+
+    printf("Modified string: %s\n", output);
+
+    return 0;
+}
