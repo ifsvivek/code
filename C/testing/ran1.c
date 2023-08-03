3ad69e2aac3ce36520477c0f52c245f98a15ba2c
ifsvivek <vs63961@gmai.com>
2023-08-03 22:33:22 +0530

    new

--- C/testing/ran1.c
+++ C/testing/ran1.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+#include<ctype.h>
+
+void replaceConstants(char *str) {
+    int i;
+    for (i = 0; i < strlen(str); i++) {
+        if (isalpha(str[i])) {
+            if (str[i] == 'a' || str[i] == 'A' || str[i] == 'z' || str[i] == 'Z')
+                continue;
+
+            str[i] = str[i] + 1;
+            if (str[i] == 'z' + 1 || str[i] == 'Z' + 1)
+                str[i] = str[i] + 1;
+        }
+    }
+}
 
 int main() {
-    float time = 2.3;
-    int hour = 0;
-    while (time >= 1) {
-        time--;
-        hour++;
-    }
-    printf("%d:%.0f\n", hour, time * 60);
+    char str[] = "programming in C is fun";
+    replaceConstants(str);
+    printf("%s\n", str);
     return 0;
 }
