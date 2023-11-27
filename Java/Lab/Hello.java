b9eda909ce47058ff9331f6dae5d0cb2dcf1098e
ifsvivek <vs63961@gmail.com>
2023-11-27 12:43:46 +0530

    previous labs

---
+++ Java/Lab/Hello.java
@@ -0,0 +1,9 @@
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Java World");
+        int a=7, b=10;
+        int c=a+b;
+        System.out.println("Sum of "+a+" and "+b+" is "+c);
+        
+    }
+}
