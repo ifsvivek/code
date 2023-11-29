2d049e853442b6c9b19989681c305d84530cadb2
ifsvivek <vs63961@gmail.com>
2023-11-29 14:32:05 +0530

    lab dsa

--- C/DSA/calender.c
+++ C/DSA/calender.c
@@ -12,6 +12,7 @@ cal* create(){
 }
 void read(cal *d){
     int i, flag;
+    char temp[50];
     for(i=0;i<7;i++){
         d[i].name=NULL;
         d[i].day=-1;
@@ -20,14 +21,17 @@ void read(cal *d){
     do{
         printf("Enter the day Number: ");
         scanf("%d",&i);
+        i--;
         if(i<0 || i>6){
             printf("Invalid Input");
         }
         else{
             d[i].day=1;
             printf("Enter the day name and description: ");
-            scanf("%s",d[i].name);
-            scanf("%s",d[i].des);
+            scanf("%s",temp);
+            d[i].name=strdup(temp);
+            scanf("%s",temp);
+            d[i].des=strdup(temp);
         }
         printf("Enter 1 for more activities else 0: ");
         scanf("%d",&flag);
@@ -37,9 +41,8 @@ void read(cal *d){
 void display(cal *d){
     printf("Day\t\tDay Number\t\tDescriptions\n");
     for (int i = 0; i <7; i++){
-        printf("%s\t\t%d\t\t%s\n",d[i].name,d[i].day,d[i].des);
-    }
-    
+        printf("%s\t\t%d\t\t\t%s\n",d[i].name,d[i].day,d[i].des);
+    }  
 }
 int main(){
     cal *d=create();
