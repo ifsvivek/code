906f5d17367f4791fe1efd03a17a3523d019ec90
ifsvivek <vs63961@gmail.com>
2023-12-04 12:19:58 +0530

    removed break

--- Java/Lab/ForEach.java
+++ Java/Lab/ForEach.java
@@ -1,12 +1,9 @@
 class Foreach {
     public static void main(String[] args) {
-        int sum=0,count=0;
+        int sum=0;
         int a[]={1,4,9,16,25,36,64,81,100};
         for (int x : a) {
-            sum=sum+x;
-            count++;
-            if(count==4)
-            break;            
+            sum=sum+x;            
         }
        System.out.println("Sum= "+sum); 
     }
