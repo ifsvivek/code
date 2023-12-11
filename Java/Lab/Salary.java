9b7add155739830fd7a160e30129995d5c73c415
ifsvivek <vs63961@gmail.com>
2023-12-11 12:13:02 +0530

    salary java

---
+++ Java/Lab/Salary.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class Employee {
+    String id, name;
+    double salary, percentage;
+    void read(){
+        Scanner s= new Scanner(System.in);
+        System.out.println("Enter ID, Name, Salary");
+        id= s.next();
+        name=s.next();
+        salary=s.nextDouble();
+    }
+    void display(){
+        System.out.println(id+"\t"+name+"\t"+salary);
+    }
+    void raise(double percentage){
+        salary+=(percentage/100);
+    }
+}
+public class Salary {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        System.out.println("Enter the number of employee");
+        int n= s.nextInt();
+        Employee e[]= new Employee[n];
+        for(int i=0; i<n;i++){
+            e[i]= new Employee();
+            e[i].read();
+        }
+        System.out.println("ID\tName\tSalary");
+        for( int i=0;i<n;i++){
+            e[i].display();
+        }
+        System.out.println("Enter percentage");
+        double per=s.nextDouble();
+        for(int i=0;i<n;i++){
+            e[i].raise(per);
+        }
+        System.out.println("ID\tName\tSalary");
+        for( int i=0;i<n;i++){
+            e[i].display();
+        }
+    }
+}
