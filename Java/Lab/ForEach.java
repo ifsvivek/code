669cf9b617161c47106fcebee399ffa4f8114568
ifsvivek <vs63961@gmail.com>
2023-12-18 12:41:16 +0530

    Lab

--- Java/Lab/ForEach.java
+++ Java/Lab/ForEach.java
@@ -1,9 +1,12 @@
-class Foreach {
+class ForEach {
     public static void main(String[] args) {
-        int sum=0;
-        int a[]={1,4,9,16,25,36,64,81,100};
+        int sum=0,count=0;
+        int a[]={10,20,30,40,50,60,70,80,90,100};
         for (int x : a) {
-            sum=sum+x;            
+            sum=sum+x;
+            count++;
+            if(count==4)
+            break;            
         }
        System.out.println("Sum= "+sum); 
     }
