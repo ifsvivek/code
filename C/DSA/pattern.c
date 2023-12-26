1783a2bf902f07291e281e428a83dca0b5a499b0
ifsvivek <vs63961@gmail.com>
2023-12-26 22:24:58 +0530

    fix?

--- C/DSA/pattern.c
+++ C/DSA/pattern.c
@@ -23,27 +23,24 @@ int findPattern(char *str, const char *pattern, int *start) {
         for (j = 0; j < patLen && str[k] == pattern[j]; j++, k++);
         if (j == patLen) {
             *start = i;
-            return 1; // Pattern found
+            return 1;
         }
     }
-    return 0; // Pattern not found
+    return 0;
 }
 
 void replacePattern(char *str, const char *pattern, const char *replaceStr) {
     int start = 0;
     while (findPattern(str, pattern, &start)) {
-        // Replace pattern with replaceStr
+
         int i, j;
         int patLen = stringLength(pattern);
         int replaceLen = stringLength(replaceStr);
         int strLen = stringLength(str);
-
-        // Shift characters to the right to make space for replaceStr
         for (i = strLen + replaceLen - 1, j = strLen - 1; j >= start + patLen; i--, j--) {
             str[i] = str[j];
         }
 
-        // Copy replaceStr to the position of the pattern
         for (i = start, j = 0; j < replaceLen; i++, j++) {
             str[i] = replaceStr[j];
         }
