41b4fed5ba430cabbfc59ad70c173bc2ca1f3a91
ifsvivek <vs63961@gmail.com>
2023-12-30 18:16:25 +0530

    check

--- C/OS/pid.c
+++ C/OS/pid.c
@@ -2,25 +2,6 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-<<<<<<< HEAD
-int main(){
-    pit_t pid;
-    pid = fork();
-    if(pid < 0){
-        printf(“fork failed”);
-        exit(0);
-    }
-    else if (pid == 0){
-        printf(“child process”);
-        execlp(“ls”,”ls”,”- l”, NULL);
-    }
-    else{
-        Wait(NULL);
-        printf(“Child Complete ”);
-        exit(0);
-    }
-}
-=======
 #include <sys/wait.h>
 
 int main(){
@@ -42,4 +23,3 @@ int main(){
         exit(0);
     }
 }
->>>>>>> 387c2e8eb643ee0af8fbf29403fc9e25f2cdc64d
