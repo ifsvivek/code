9d7ddb4646f10e77b925a8edd2e1d4055712abc9
ifsvivek <vs63961@gmail.com>
2023-12-30 21:18:53 +0530

    fix

--- C/DSA/tower.c
+++ C/DSA/tower.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-void towerOfHanoi(int n, char source, char auxiliary, char destination) {
+void tower(int n, char source, char auxiliary, char destination) {
     if (n == 1) {
         printf("Move disk 1 from %c to %c\n", source, destination);
         return;
     }
 
-    towerOfHanoi(n - 1, source, destination, auxiliary);
+    tower(n - 1, source, destination, auxiliary);
     printf("Move disk %d from %c to %c\n", n, source, destination);
-    towerOfHanoi(n - 1, auxiliary, source, destination);
+    tower(n - 1, auxiliary, source, destination);
 }
 
 int main() {
@@ -23,7 +23,7 @@ int main() {
     }
 
     printf("Tower of Hanoi solution for %d disks:\n", n);
-    towerOfHanoi(n, 'A', 'B', 'C');
+    tower(n, 'A', 'B', 'C');
 
     return 0;
 }
