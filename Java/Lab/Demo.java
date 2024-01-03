76db1ff7fe81fe4305135456c4df38a824aaddf9
ifsvivek <vs63961@gmail.com>
2024-01-03 14:37:42 +0530

    sync

--- Java/Lab/Demo.java
+++ Java/Lab/Demo.java
@@ -1,70 +1,82 @@
 import java.util.Scanner;
-class Staff{
-    String sid,name,phone,salary;
-    Scanner s=new Scanner(System.in);
-    void read(){
+
+class Staff {
+    String sid, name, phone, salary;
+    Scanner s = new Scanner(System.in);
+
+    void read() {
         System.out.println("Enter SID,Name,Phone No.,Salary");
-        sid=s.nextLine();
-        name=s.nextLine();
-        phone=s.nextLine();
-        salary=s.nextLine();
+        sid = s.nextLine();
+        name = s.nextLine();
+        phone = s.nextLine();
+        salary = s.nextLine();
     }
-    void display(){
-        System.out.print(sid+"\t"+name+"\t"+phone+"\t"+salary+"\t");
+
+    void display() {
+        System.out.print(sid + "\t" + name + "\t" + phone + "\t" + salary + "\t");
     }
 }
 
-class Teaching extends Staff{
-    String domain,publication;
-    void read(){
+class Teaching extends Staff {
+    String domain, publication;
+
+    void read() {
         super.read();
         System.out.println("Enter domain, publication");
-        domain=s.nextLine();
-        publication=s.nextLine();
+        domain = s.nextLine();
+        publication = s.nextLine();
     }
-    void display(){
+
+    void display() {
         super.display();
-        System.out.print(domain+"\t"+publication+"\n");
+        System.out.print(domain + "\t" + publication + "\n");
     }
 }
-class Technical extends Staff{
+
+class Technical extends Staff {
     String skill;
-    void read(){
+
+    void read() {
         super.read();
         System.out.println("Enter skill");
-        skill=s.nextLine();
+        skill = s.nextLine();
     }
-    void display(){
+
+    void display() {
         super.display();
-        System.out.print(skill+"\n");
+        System.out.print(skill + "\n");
     }
 }
-class Contract extends Staff{
+
+class Contract extends Staff {
     String period;
-    void read(){
+
+    void read() {
         super.read();
         System.out.println("Enter period");
-        period=s.nextLine();
+        period = s.nextLine();
     }
-    void display(){
+
+    void display() {
         super.display();
-        System.out.print(period+"\n");
+        System.out.print(period + "\n");
     }
 }
+
 public class Demo {
     public static void main(String[] args) {
-        Teaching t=new Teaching();
+        Teaching t = new Teaching();
         t.read();
         System.out.println("SID\tName\tPhone No.\tSalary\tDomain\tPublication");
         t.display();
-        Technical th=new Technical();
+        Technical th = new Technical();
         th.read();
         System.out.println("SID\tName\tPhone No.\tSalary\tSkill");
         th.display();
-        Contract c=new Contract();
+        Contract c = new Contract();
         c.read();
         System.out.println("SID\tName\tPhone No.\tSalary\tPeriod");
         c.display();
     }
-    
+
 }
