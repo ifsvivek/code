bbae665f3a63ddaa6581e267f8b9157af7ecd352
ifsvivek <vs63961@gmail.com>
2024-01-14 18:22:47 +0530

    format

--- C/DSA/sll.c
+++ C/DSA/sll.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct sll
-{
+struct sll{
     int sem;
     char usn[11], name[20], branch[20], phone[11];
     struct sll *next;
 };
-
 typedef struct sll node;
-
 node *start = NULL;
 
-node *getnode()
-{
+node *getnode(){
     node *newNode;
     newNode = (node *)malloc(sizeof(node));
     printf("Enter the student details USN, name, branch, sem, phone: ");
@@ -22,171 +18,101 @@ node *getnode()
     return newNode;
 }
 
-void insertBegin()
-{
+void insertBegin(){
     node *newNode = getnode();
 
     if (start == NULL)
-    {
         start = newNode;
-    }
-    else
-    {
+    else{
         newNode->next = start;
         start = newNode;
     }
 }
 
-void insertEnd()
-{
+void insertEnd(){
     node *newNode = getnode();
     node *temp = start;
 
     if (start == NULL)
-    {
         start = newNode;
-    }
-    else
-    {
+    else{
         while (temp->next != NULL)
-        {
             temp = temp->next;
-        }
-
         temp->next = newNode;
     }
 }
 
-void deleteBegin()
-{
-    node *temp;
+void deleteBegin(){
+    node *temp=start;
 
     if (start == NULL)
-    {
         printf("Linked list is empty.\n");
-    }
-    else
-    {
-        temp = start;
+    else{
+        printf("Deleted node: USN: %s\n", temp->usn);
         start = temp->next;
-        printf("Deleted node: USN: %s\n", temp->usn);
         free(temp);
     }
 }
 
-void deleteEnd()
-{
-    node *temp, *prev;
+void deleteEnd(){
+    node *temp=start, *prev;
 
     if (start == NULL)
-    {
-        printf("Linked list is empty.\n");
-    }
-    else
-    {
-        temp = start;
-        prev = NULL;
-
-        while (temp->next != NULL)
-        {
-            prev = temp;
-            temp = temp->next;
-        }
-
-        printf("Deleted node: USN: %s\n", temp->usn);
-
-        if (prev != NULL)
-        {
-            prev->next = NULL;
-        }
-        else
-        {
-            start = NULL; // If the list had only one node
-        }
-
+        printf("List is empty.\n");
+    if (start->next==NULL){
+        printf("Deleted node is %s",temp->usn);
+        start=NULL;
         free(temp);
+    }else{
+        while (temp->next!=NULL){
+            prev=temp;
+            temp=temp->next;
+        }
+    prev->next=NULL;
+    printf("Deleted node is %s",temp->usn);
+    free(temp);
     }
 }
 
-void displayAndCount()
-{
+void display(){
     node *temp = start;
     int count = 0;
 
     if (start == NULL)
-    {
         printf("List is empty.\n");
-    }
-    else
-    {
+    else{
         printf("List elements are:\n");
-        while (temp != NULL)
-        {
-            printf("USN: %s, Name: %s, Branch: %s, Sem: %d, Phone: %s\n",
-                   temp->usn, temp->name, temp->branch, temp->sem, temp->phone);
-            temp = temp->next;
+        while (temp != NULL){
+            printf("%s",temp->usn);
             count++;
         }
         printf("Count = %d\n", count);
     }
 }
 
-int main()
-{
+void main(){
     int choice, n, i;
 
-    while (1)
-    {
-        printf("\n***** Singly Linked List Operations *****\n");
-        printf("1. Insert at Beginning\n");
-        printf("2. Insert at End\n");
-        printf("3. Delete from Beginning\n");
-        printf("4. Delete from End\n");
-        printf("5. Display and Count\n");
-        printf("6. Exit\n");
-
-        printf("Enter your choice: ");
+    while (1){
+        printf("1.  InsertBegin\n2.  InsertEnd\n3.  DeleteBegin\n4.  DeleteEnd\n5.  Display\n6.  Exit\n");
         scanf("%d", &choice);
-
-        switch (choice)
-        {
-        case 1:
-            printf("Enter the number of students to insert at the beginning: ");
-            scanf("%d", &n);
-            for (i = 0; i < n; i++)
-            {
-                insertBegin();
-            }
-            break;
-
-        case 2:
-            printf("Enter the number of students to insert at the end: ");
-            scanf("%d", &n);
-            for (i = 0; i < n; i++)
-            {
-                insertEnd();
-            }
-            break;
-
-        case 3:
-            deleteBegin();
-            break;
-
-        case 4:
-            deleteEnd();
-            break;
-
-        case 5:
-            displayAndCount();
-            break;
-
-        case 6:
-            exit(0);
-
+        switch (choice){
+        case 1: printf("Enter the no. students: ");
+                scanf("%d", &n);
+                for (i = 0; i < n; i++)
+                    insertBegin();
+                break;
+        case 2: insertEnd();
+                break;
+        case 3: deleteBegin();
+                break;
+        case 4: deleteEnd();
+                break;
+        case 5: display();
+                break;
+        case 6: exit(0);
         default:
-            printf("Invalid choice. Please enter a valid option.\n");
+            printf("Invalid choice.\n");
         }
     }
-
-    return 0;
 }
