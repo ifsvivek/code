63a020d0c2e8dc2b66762c5afdd31fc396e7821a
ifsvivek <vs63961@gmail.com>
2024-01-15 11:29:40 +0530

    sync

--- C/testing/ran10.c
+++ C/testing/ran10.c
@@ -1,42 +1,45 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
-{
+int main() {
     char str[100], pat[100], rep[100], ans[100];
 
     printf("Enter the original string \n");
     fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';
+    
     printf("Enter the pattern string \n");
-    fgets(pat, sizeof(str), stdin);
+    fgets(pat, sizeof(pat), stdin);
+    pat[strcspn(pat, "\n")] = '\0';
+
     printf("Enter the replacement string \n");
-    fgets(rep, sizeof(str), stdin);
+    fgets(rep, sizeof(rep), stdin);
+    rep[strcspn(rep, "\n")] = '\0';
 
     int i, j, c, m, k;
-    i, c, m, j = 0;
-    while (str[c] != '\0')
-    {
-        if (str[m] == pat[i])
-        {
+
+    i = c = m = j = 0;
+
+    while (str[c] != '\0') {
+        if (str[c] == pat[i]) {
             i++;
-            m++;
-        }
-        if (pat[i] == '\0')
-        {
-            for (k = 0; rep[k] != '\0'; k++, j++)
-            {
-                ans[j] = rep[k];
+            c++;
+        } else {
+            if (i > 0) {
+                c = m + i;
+                i = 0;
+            } else {
+                ans[j++] = str[m++];
+                c = m;
             }
-            i++;
-            c = m;
         }
 
-        else
-        {
-            ans[j] = str[c];
-            j++;
-            c++;
-            m = c;
+        if (pat[i] == '\0') {
+            for (k = 0; rep[k] != '\0'; k++, j++) {
+                ans[j] = rep[k];
+            }
             i = 0;
+            m = c;
         }
     }
 
