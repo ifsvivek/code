0664569bf383ec3ca1854d8a7b2c20cb42186723
ifsvivek <vs63961@gmail.com>
2024-01-16 15:49:30 +0530

    sync

---
+++ Java/testing/Test8.java
@@ -0,0 +1,51 @@
+abstract class Shape {
+    abstract void draw();
+
+    abstract void erase();
+}
+
+class Circle extends Shape {
+    void draw() {
+        System.out.println("Circle Created");
+    }
+
+    void erase() {
+        System.out.println("Circle Erased");
+    }
+}
+
+class Triangle extends Shape {
+    void draw() {
+        System.out.println("Triangle Created");
+    }
+
+    void erase() {
+        System.out.println("Triangle Erased");
+    }
+}
+
+class Square extends Shape {
+    void draw() {
+        System.out.println("Square Created");
+    }
+
+    void erase() {
+        System.out.println("Square Erased");
+    }
+}
+
+public class Test8 {
+    public static void main(String[] args) {
+            Shape obj=new Circle();
+            obj.draw();
+            obj.erase();
+            obj=new Triangle();
+            obj.draw();
+            obj.erase();
+            obj=new Square();
+            obj.draw();
+            obj.erase();
+
+    }
+
+}
