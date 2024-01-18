49af411664cb6c9618226385052bd7d56974967f
ifsvivek <vs63951@gmail.com>
2024-01-18 09:45:45 +0530

    sync

--- C/OS/sjf.c
+++ C/OS/sjf.c
@@ -1,31 +1,30 @@
-SJF Scheduling algorithm
 #include<stdio.h>
-int main() {
-  int time, burst_time[10], at[10], sum_burst_time = 0, smallest, n, i;
-  int sumt = 0, sumw = 0;
-  printf("enter the no of processes : ");
-  scanf("%d", & n);
-  for (i = 0; i < n; i++) {
-    printf("the arrival time for process P%d : ", i + 1);
-    scanf("%d", & at[i]);
-    printf("the burst time for process P%d : ", i + 1);
-    scanf("%d", & burst_time[i]);
-    sum_burst_time += burst_time[i];
-  }
-  burst_time[9] = 9999;
-  for (time = 0; time < sum_burst_time;) {
-    smallest = 9;
-    for (i = 0; i < n; i++) {
-      if (at[i] <= time && burst_time[i] > 0 && burst_time[i] < burst_time[smallest])
-        smallest = i;
-    }
-  printf("P[%d]\t|\t%d\t|\t%d\n", smallest + 1, time + burst_time[smallest] - at[smallest], time - at[smallest]);
-    sumt += time + burst_time[smallest] - at[smallest];
-    sumw += time - at[smallest];
-    time += burst_time[smallest];
-    burst_time[smallest] = 0;
-  }
-  printf("\n\n average waiting time = %f", sumw * 1.0 / n);
-  printf("\n\n average turnaround time = %f", sumt * 1.0 / n);
-  return 0;
+int main(){
+	int time,bt[10],at[10],sbt=0,smallest,n,i;
+	float sumt=0,sumw=0;
+	printf("Enter the no. of processes: ");
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		printf("The AT of Process P%d: ",i+1);
+		scanf("%d",&at[i]);
+		printf("The BT of Process P%d: ",i+1);
+		scanf("%d",&bt[i]);
+	}
+	bt[9]=9999;
+	for(time=0;time<sbt;){
+		smallest=9;
+		for(i=0;i<n;i++){
+			if(at[i]<=time && bt[i]>0 && bt[i]<bt[smallest])
+				smallest=i;
+		}
+		printf("P[%d]\t|\t%d\t|\t%d\n",smallest+1,time+bt[smallest]-at[smallest],time-at[smallest]);
+		sumt+=time+bt[smallest]-at[smallest];
+		sumw+=time-at[smallest];
+		time+=bt[smallest];
+		bt[smallest]=0;
+	}
+	printf("\n\nAWT=%f\n",sumw*1.0/n);
+	printf("\n\nATAT=%f\n",sumt*1.0/n);
+	return 0;
 }
+
