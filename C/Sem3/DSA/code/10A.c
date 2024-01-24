d4c2f16e583a3a5fe4d81ce945976e30594ed76e
ifsvivek <vs63961@gmail.com>
2024-01-24 22:39:36 +0530

    sync

---
+++ C/Sem3/DSA/code/10A.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to display the value of the last but one node in CSLL
+void display(Node* head) {
+    if (head == NULL || head->next == NULL) {
+        printf("List is empty.\n");
+        return;
+    }
+    Node* current = head;
+    while (current->next->next != head)
+        current = current->next;
+    printf("Value of the last but one node: %d\n", current->data);
+}
+
+
+void main() {
+
+    Node* head = createNode(1);
+    head->next = createNode(2);
+    head->next->next = createNode(3);
+    head->next->next->next = head;
+
+    display(head);
+}
---
+++ C/Sem3/DSA/code/10B.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* prev;
+    struct Node* next;
+} Node;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to display the sum of the first and last node values in DLL
+void display(Node* head) {
+    if (head == NULL) {
+        printf("List is empty.\n");
+        return;
+    }
+
+    Node* current = head;
+    while (current->next != NULL)
+        current = current->next;
+    printf("Sum of first and last node values: %d\n", head->data + current->data);
+}
+
+
+void main() {
+
+    Node* head = createNode(1);
+    head->next = createNode(2);
+    head->next->prev = head;
+    head->next->next = createNode(3);
+    head->next->next->prev = head->next;
+
+    display(head);
+}
