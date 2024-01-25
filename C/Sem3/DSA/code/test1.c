acc4358ff4b9323632fab178c2c54dc0f1e74340
ifsvivek <vs63961@gmail.com>
2024-01-25 21:34:01 +0530

    sync

--- C/Sem3/DSA/code/test1.c
+++ C/Sem3/DSA/code/test1.c
@@ -1,366 +1,343 @@
-//SINGLE LINKED LIST//
-//CREATE NODE//
-struct SLL
-{
+#include<stdio.h>
+#include<stdlib.h>
+
+// SINGLE LINKED LIST //
+// CREATE NODE //
+struct SLL {
     int data;
     struct SLL *next;
-}
+};
+
 typedef struct SLL node;
-node *start=NULL;
-node *createnode()
-{
+node *start = NULL;
+
+node *createnode() {
     node *newnode;
-    newnode=(node*)malloc(sizeof(node));
-    printf("ENTER THE DATA : ");
-    scanf("%d",&newnode->data);
-    newnode->next=NULL;
+    newnode = (node*)malloc(sizeof(node));
+    printf("Enter the data: ");
+    scanf("%d", &newnode->data);
+    newnode->next = NULL;
     return newnode;
 }
-//INSERT BEGIN/FRONT//
-void insertBegin()
-{
-    node *newnode=createnode();
-    if(start==NULL)
-    {
-        start=newnode;
+
+// INSERT BEGIN / FRONT //
+void insertBegin() {
+    node *newnode = createnode();
+    if (start == NULL) {
+        start = newnode;
         return;
     }
-    newnode->next=start;
-    start=newnode;
+    newnode->next = start;
+    start = newnode;
 }
-//INSERT END/REAR//
-void insertEnd()
-{
-    node *temp=start;
-    node *newnode=createnode();
-    if(start==NULL)
-    {
-        start=newnode;
+
+// INSERT END / REAR //
+void insertEnd() {
+    node *temp = start;
+    node *newnode = createnode();
+    if (start == NULL) {
+        start = newnode;
         return;
     }
-    while(temp->next!=NULL)
-        temp=temp->next;
-    temp->next=newnode;
+    while (temp->next != NULL)
+        temp = temp->next;
+    temp->next = newnode;
 }
-//DELETE BEGIN//
-void deleteBegin()
-{
-    node *temp=start;
-    if(start==NULL)
-    {
-        printf("LIST IS EMPTY");
+
+// DELETE BEGIN //
+void deleteBegin() {
+    node *temp = start;
+    if (start == NULL) {
+        printf("List is empty\n");
         return;
     }
-    start=temp->next;
-    printf("DELETED ELEMENT IS %d ",temp->data);
+    start = temp->next;
+    printf("Deleted element is %d\n", temp->data);
     free(temp);
 }
-//DELETE END//
-void deleteEnd()
-{
-    node *temp=start,*prev;
-    if(start==NULL)
-        printf("LIST IS EMPTY");
-    else if(start->next==NULL)
-    {
-        printf("DELETED NODE IS %d "start->data);
+
+// DELETE END //
+void deleteEnd() {
+    node *temp = start, *prev = NULL;
+    if (start == NULL) {
+        printf("List is empty\n");
+        return;
+    } else if (start->next == NULL) {
+        printf("Deleted node is %d\n", start->data);
         free(temp);
-        start=NULL;
-    }
-    else
-    {
-        while(temp->next!=NULL)
-        {
-            prev=temp;
-            temp=temp->next;
+        start = NULL;
+    } else {
+        while (temp->next != NULL) {
+            prev = temp;
+            temp = temp->next;
         }
-        printf("DELETED NODE IS %d ",temp->data);
+        printf("Deleted node is %d\n", temp->data);
         free(temp);
-        prev->next=NULL;
+        prev->next = NULL;
     }
 }
-//DISPLAY//
-void display()
-{
-    if(start==NULL)
-    {
-        printf("LIST IS EMPTY");
+// DISPLAY //
+void display() {
+    node *temp = start;
+    if (start == NULL) {
+        printf("List is empty\n");
         return;
     }
-    while(temp!=NULL)
-    {
-        printf("%d\t",temp->data);
-        temp=temp->next;
+    while (temp != NULL) {
+        printf("%d\t", temp->data);
+        temp = temp->next;
     }
 }
-//SEARCH//
-void search()
-{
+
+// SEARCH //
+void search() {
     int key;
-    node *temp=start;
-    if(start==NULL)
-    {
-        printf("LIST IS EMPTY");
+    node *temp = start;
+    if (start == NULL) {
+        printf("List is empty\n");
         return;
     }
-    printf("ENTER THE KEY : ");
-    scanf("%d",&key);
-    while(temp!=NULL&&temp->data)
-        temp=temp->next;
-    if(temp==NULL)
-        printf("KEY NOT FOUND");
+    printf("Enter the key: ");
+    scanf("%d", &key);
+    while (temp != NULL && temp->data != key)
+        temp = temp->next;
+    if (temp == NULL)
+        printf("Key not found\n");
     else
-        printf("KEY FOUND");
+        printf("Key found\n");
 }
-//INSERT BEFORE KEY//
-void insertbeforekey()
-{
-    node *temp=start,*prev,*newnode;
+
+// INSERT BEFORE KEY //
+void insertbeforekey() {
+    node *temp = start, *prev = NULL, *newnode;
     int key;
-    if(start==NULL)
-    {
-        printf("LIST IS EMPTY");
+    if (start == NULL) {
+        printf("List is empty\n");
         return;
     }
-    printf("ENTER THE KEY");
-    scanf("%d",&key);
-    while(temp!=NULL&&temp->data!=key)
-    {
-        prev=temp;
-        temp=temp->next;
+    printf("Enter the key: ");
+    scanf("%d", &key);
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
     }
-    if(temp==NULL)
-    {
-        printf("INSERTED KEY");
+    if (temp == NULL) {
+        printf("Key not found\n");
         return;
     }
-    if(temp==start)
-    {
+    if (temp == start) {
         insertBegin();
         return;
     }
-    newnode=createnode();
-    prev->next=newnode;
-    newnode->next=temp;
+    newnode = createnode();
+    prev->next = newnode;
+    newnode->next = temp;
 }
-//INSERT AFTER KEY//
-void insertafterkey()
-{
-    node *temp=start,*newnode;
+
+// INSERT AFTER KEY //
+void insertafterkey() {
+    node *temp = start, *newnode, *prev = NULL;
     int key;
-    if(start==NULL)
-    {
-        printf("LIST IS EMPTY");
+    if (start == NULL) {
+        printf("List is empty\n");
         return;
     }
-    printf("ENTER THE KEY");
-    scanf("%d",&key);
-    while(temp!=NULL&&temp->data!=key)
-        temp=temp->next;
-    if(temp==NULL)
-    {
-        printf("INSERTED KEY");
+    printf("Enter the key: ");
+    scanf("%d", &key);
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+    if (temp == NULL) {
+        printf("Key not found\n");
         return;
     }
-    newnode=createnode();
-    prev->next=newnode;
-    newnode->next=temp;
+    newnode = createnode();
+    newnode->next = temp->next;
+    temp->next = newnode;
+    printf("Key inserted\n");
 }
-//DELETE BEFORE KEY//
-void deletebeforekey()
-{
-    node *temp=start,*prev;
+
+// DELETE BEFORE KEY //
+void deletebeforekey() {
+    node *temp = start, *prev = NULL;
     int key;
-    if(start==NULL)
-    {
-        printf("LIST IS EMPTY");
+    if (start == NULL) {
+        printf("List is empty\n");
         return;
     }
-    printf("ENTER THE KEY");
-    scanf("%d",&key);
-    while(temp->next!=NULL&&(temp->next)->data!=key)
-    {
-        temp->next=NULL;
-        prev=temp;
-        temp=temp->next;
+    printf("Enter the key: ");
+    scanf("%d", &key);
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
     }
-    if(temp==NULL)
-    {
-        printf("KEY NOT FOUND");
+    if (temp == NULL) {
+        printf("Key not found\n");
         return;
     }
-    if(temp==start)
-    {
-        printf("DELETION IS NOT POSSIBLE AS NO NODES PRESENT BEFORE");
+    if (prev == NULL || prev == start) {
+        printf("Deletion is not possible as no nodes present before\n");
         return;
     }
-    if(prev==start)
-    {
+    prev->next = temp->next;
+    printf("Deleted element is %d\n", temp->data);
+    free(temp);
+}
+
+// DELETE AFTER KEY //
+void deleteafterkey() {
+    node *temp = start, *prev = NULL;
+    int key;
+    if (start == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    printf("Enter the key: ");
+    scanf("%d", &key);
+    while (temp != NULL && temp->data != key)
+        temp = temp->next;
+    if (temp == NULL) {
+        printf("Deletion not possible as no key found\n");
+        return;
+    }
+    if (temp->next == NULL) {
+        printf("No node after the key to delete\n");
+        return;
+    }
+    node *nextNode = temp->next;
+    temp->next = nextNode->next;
+    printf("Deleted element is %d\n", nextNode->data);
+    free(nextNode);
+}
+
+// CONCATENATING LISTS //
+node* concat(node* start1, node* start2) {
+    node *temp;
+    if (start1 == NULL && start2 == NULL) {
+        printf("Concatenation is not possible because both lists are empty\n");
+        return NULL;
+    }
+    if (start1 == NULL)
+        return start2;
+    if (start2 == NULL)
+        return start1;
+    temp = start1;
+    while (temp->next != NULL)
+        temp = temp->next;
+    temp->next = start2;
+    return start1;
+}
+
+// DELETE KEY //
+void deletekey() {
+    node *temp = start, *prev = NULL;
+    int key;
+    if (start == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    printf("Enter the key: ");
+    scanf("%d", &key);
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+    if (temp == NULL) {
+        printf("Key not found\n");
+        return;
+    }
+    if (prev == NULL) {
         deleteBegin();
         return;
     }
-    prev->next=temp->next;
-    printf("DELETED ELEMENT IS %d ",temp->data);
+    prev->next = temp->next;
+    printf("Deleted key is %d\n", temp->data);
     free(temp);
 }
-//DELETE AFTER//
-void deleteafter()
-{
-    node *temp=start,*prev;
-    int key;
-    if(start==NULL)
-    {
-        printf("KEY NOT FOUND");
+
+// REVERSE A LINKED LIST //
+void reverse() {
+    node *temp = start, *prev = NULL, *next = NULL;
+    if (start == NULL) {
+        printf("List is empty\n");
         return;
     }
-    while(temp!=NULL&&temp->data!=key)
-        temp=temp->next;
-    if(temp==NULL)
-    {
-        printf("DELETION NOT POSSIBLE AS NO KEY FOUND");
-        return;
+    while (temp != NULL) {
+        next = temp->next;
+        temp->next = prev;
+        prev = temp;
+        temp = next;
     }
-    temp->next=(temp->next)->next;
-    printf("DELETED ELEMENT IS %d",(temp->next)->data);
-    free(temp->next);
+    start = prev;
 }
-//CONCATENATING LISTS//
-node *concat(node *start1,node *start2)
-{
-    node *temp;
-    if(start1==NULL&&start2==NULL)
-    {
-        printf("CANCAT IS NOT POSSIBLE BECAUSE BOTH LIST ARE EMPTY");
+
+// SORT //
+void sort() {
+    int n = 0, tdata, i, j;
+    node *temp = start;
+    if (start == NULL) {
+        printf("List is empty\n");
         return;
     }
-    if(start1==NULL)
-        return start2;
-    if(start2==NULL)
-        return start1;
-    temp=start1;
-    while(temp->next!=NULL)
-        temp=temp->next;
-    temp->next=start2;
-    return start1;
-}
-//DELETE KEY//
-void deletekey()
-{
-    node *temp=start,*prev;
-    int key;
-    if(start==NULL)
-    {
-        printf("LIST IS EMPTY");
-        return;
-    }
-    printf("ENTER THE KEY");
-    scanf("%d",&key);
-    while(temp!=NULL&&temp->data!=key)
-    {
-        prev=temp;
-        temp=temp->next;
-    }
-    if(temp==NULL)
-    {
-        printf("KEY NOT FOUND");
-        return;
-    }
-    if(temp==start)
-    {
-        deletebegin();
-        return;
-    }
-    prev->next=temp->next;
-    printf("DELETED KEY IS %d",temp->data);
-    free(temp);
-}
-//REVERSE A LINKED LIST//
-void reverse(node *start)
-{
-    if(start==NULL)
-        printf("LIST IS EMPTY");
-    while(start!=NULL)
-    {
-        temp=start;
-        start=temp->next;
-        temp->next=prev;
-        prev=temp;
-    }
-    start=temp;
-    free(temp);
-}
-//SORT//
-void sort()
-{
-    int n=0,tdata,i,j;
-    node *temp=start;
-    if(start==NULL)
-    {
-        printf("LIST IS EMPTY");
-        return;
-    }
-    while(temp!=NULL)
-    {
+    while (temp != NULL) {
         n++;
-        temp=temp->next;
+        temp = temp->next;
     }
-    for(i=0;i<n-1;i++)
-    {
-        temp=start;
-        for(j=0;j<n-1;j++)
-        {
-            if(temp->data>(temp->next)-data)
-            {
-                temp=temp->data;
-                temp->data=(temp->next)->data;
-                (temp->next)->data=tdata;
+    for (i = 0; i < n - 1; i++) {
+        temp = start;
+        for (j = 0; j < n - 1; j++) {
+            if (temp->data > temp->next->data) {
+                tdata = temp->data;
+                temp->data = temp->next->data;
+                temp->next->data = tdata;
             }
-            temp=temp->next;
+            temp = temp->next;
         }
     }
     display();
 }
-//COUNTING NUMBER OF NODES//
-void count(node *count)
-{
-    while(temp!=NULL)
-    {
+
+// COUNTING NUMBER OF NODES //
+int count() {
+    int count = 0;
+    node *temp = start;
+    while (temp != NULL) {
         count++;
-        temp=temp->next;
+        temp = temp->next;
     }
+    return count;
 }
-int main() 
-{
-    int choice, value;
-    while(1) 
-    {
+
+void main() {
+    int choice;
+    while (1) {
         printf("\n\n------ Singly Linked List Menu ------\n");
         printf("1. Create node\n");
-        printf("2. Insert begin\n");
-        printf("3. Insert at the End\n");
-        printf("4. Delete from the Beginning\n");
-        printf("5. Delete from the End\n");
-        printf("6. Display the List\n");
+        printf("2. Insert at the beginning\n");
+        printf("3. Insert at the end\n");
+        printf("4. Delete from the beginning\n");
+        printf("5. Delete from the end\n");
+        printf("6. Display the list\n");
         printf("7. Search\n");
         printf("8. Insert before key\n");
         printf("9. Insert after key\n");
         printf("10. Delete before key\n");
         printf("11. Delete after key\n");
-        printf("12. Concatenating the list\n");
+        printf("12. Concatenate the list\n");
         printf("13. Delete key\n");
         printf("14. Reverse the linked list\n");
         printf("15. Sort the linked list\n");
-        printf("16. Count\n");
+        printf("16. Count nodes\n");
         printf("17. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-        switch (choice) 
-        {
+        switch (choice) {
             case 1:
                 createnode();
                 break;
             case 2:
-                insertbegin();
+                insertBegin();
                 break;
             case 3:
                 insertEnd();
@@ -369,7 +346,7 @@ int main()
                 deleteBegin();
                 break;
             case 5:
-                deleteend();
+                deleteEnd();
                 break;
             case 6:
                 display();
@@ -390,7 +367,8 @@ int main()
                 deleteafterkey();
                 break;
             case 12:
-                concat();
+                // Assuming you have two lists, modify this accordingly
+                start = concat(start, start); 
                 break;
             case 13:
                 deletekey();
@@ -402,11 +380,11 @@ int main()
                 sort();
                 break;
             case 16:
-                count();
+                printf("Number of nodes: %d\n", count());
                 break;
             case 17:
                 printf("Exiting the program.\n");
-                break;
+                exit(0);
             default:
                 printf("Invalid choice. Please try again.\n");
         }
