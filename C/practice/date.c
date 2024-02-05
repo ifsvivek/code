315b95ffb1f0896f4fd4a07acb2de134a04d2718
ifsvivek <vs63961@gmail.com>
2024-02-05 08:37:53 +0530

    sync

--- C/practice/date.c
+++ C/practice/date.c
@@ -1,36 +1,34 @@
 // print days of the week
 
 #include <stdio.h>
-void main(){
-    int day;
-    printf("Enter the day number: ");
-    scanf("%d", &day);
-    switch (day)
-    {
+void main() {
+  int day;
+  printf("Enter the day number: ");
+  scanf("%d", &day);
+  switch (day) {
     case 1:
-        printf("Monday");
-        break;
+      printf("Monday");
+      break;
     case 2:
-        printf("Tuesday");
-        break;
+      printf("Tuesday");
+      break;
     case 3:
-        printf("Wednesday");
-        break;
+      printf("Wednesday");
+      break;
     case 4:
-        printf("Thursday");
-        break;
+      printf("Thursday");
+      break;
     case 5:
-        printf("Friday");
-        break;
+      printf("Friday");
+      break;
     case 6:
-        printf("Saturday");
-        break;
+      printf("Saturday");
+      break;
     case 7:
-        printf("Sunday");
-        break;
+      printf("Sunday");
+      break;
     default:
-        printf("Invalid day");
-        break;
-    }
-    
+      printf("Invalid day");
+      break;
+  }
 }
