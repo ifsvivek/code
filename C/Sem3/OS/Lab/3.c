cc7c101b24fab3bf634c94265260cf38981a3d86
ifsvivek <vs63961@gmail.com>
2024-02-14 23:36:46 +0530

    sdfghj

--- C/Sem3/OS/Lab/3.c
+++ C/Sem3/OS/Lab/3.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 int mutex = 1, full = 0, empty = 3, x = 0;
+
+int wait(int s) {
+  return (--s);
+}
+int signal(int s) {
+  return (++s);
+}
+void producer() {
+  mutex = wait(mutex);
+  full = signal(full);
+  empty = wait(empty);
+  x++;
+  printf("\nProducer produces the item%d", x);
+  mutex = signal(mutex);
+}
+void consumer() {
+  mutex = wait(mutex);
+  full = wait(full);
+  empty = signal(empty);
+  printf("\nConsumer consumes item%d", x);
+  x--;
+  mutex = signal(mutex);
+}
 void main() {
   int n;
-  void producer();
-  void consumer();
-  int wait(int);
-  int signal(int);
   printf("\n1.PRODUCER\n2.CONSUMER\n3.EXIT\n");
   while (1) {
     printf("\nENTER YOUR CHOICE\n");
@@ -29,28 +48,6 @@ void main() {
         break;
     }
   }
-}
-int wait(int s) {
-  return (--s);
-}
-int signal(int s) {
-  return (++s);
-}
-void producer() {
-  mutex = wait(mutex);
-  full = signal(full);
-  empty = wait(empty);
-  x++;
-  printf("\nproducer produces the item%d", x);
-  mutex = signal(mutex);
-}
-void consumer() {
-  mutex = wait(mutex);
-  full = wait(full);
-  empty = signal(empty);
-  printf("\n consumer consumes item%d", x);
-  x--;
-  mutex = signal(mutex);
 }
 
 /*
