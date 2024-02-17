5eef1d7e9861c3a416b2002b2bddca1b677ba4a7
ifsvivek <vs63961@gmail.com>
2024-02-18 00:43:20 +0530

    dfgh

--- C/Sem3/OS/Lab/9.c
+++ C/Sem3/OS/Lab/9.c
@@ -2,16 +2,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-void main()
-{
+void main() {
   int f[50], p, i, st, len, j, c, k, a;
   for (i = 0; i < 50; i++)
     f[i] = 0;
   printf("Enter how many blocks already allocated: ");
   scanf("%d", &p);
   printf("Enter blocks already allocated: ");
-  for (i = 0; i < p; i++)
-  {
+  for (i = 0; i < p; i++) {
     scanf("%d", &a);
     f[a] = 1;
   }
@@ -19,23 +17,17 @@ x:
   printf("Enter index starting block and length: ");
   scanf("%d%d", &st, &len);
   k = len;
-  if (f[st] == 0)
-  {
-    for (j = st; j < (st + k); j++)
-    {
-      if (f[j] == 0)
-      {
+  if (f[st] == 0) {
+    for (j = st; j < (st + k); j++) {
+      if (f[j] == 0) {
         f[j] = 1;
         printf("%d-------->%d\n", j, f[j]);
-      }
-      else
-      {
+      } else {
         printf("%d Block is already allocated\n", j);
         k++;
       }
     }
-  }
-  else
+  } else
     printf("%d starting block is already allocated\n", st);
   printf("Do you want to enter more file(Yes - 1/No - 0)");
   scanf("%d", &c);
