320bc5498ccf1ac90fc4ca76e2c5e25538a45dbb
ifsvivek <vs63961@gmail.com>
2024-02-17 23:14:19 +0530

    sync

--- C/Sem3/OS/Lab/6A.c
+++ C/Sem3/OS/Lab/6A.c
@@ -1,37 +1,34 @@
 #include <stdio.h>
-#define max 25
 void main() {
-  int frag[max], b[max], f[max], i, j, nb, nf, temp;
-  static int bf[max], ff[max];
-  printf("\n\tMemory Management Scheme - First Fit");
-  printf("\nEnter the number of blocks:");
-  scanf("%d", &nb);
-  printf("Enter the number of files:");
-  scanf("%d", &nf);
-  printf("\nEnter the size of the blocks:-\n");
-  for (i = 1; i <= nb; i++) {
-    printf("Block %d:", i);
-    scanf("%d", &b[i]);
+  int bsize[10], psize[10], bno, pno, flags[10], allocation[10], i, j;
+  for (i = 0; i < 10; i++) {
+    flags[i] = 0;
+    allocation[i] = -1;
   }
-  printf("Enter the size of the files :-\n");
-  for (i = 1; i <= nf; i++) {
-    printf("File %d:", i);
-    scanf("%d", &f[i]);
-  }
-  for (i = 1; i <= nf; i++) {
-    for (j = 1; j <= nb; j++) {
-      if (bf[j] != 1) {
-        temp = b[j] - f[i];
-        if (temp >= 0) {
-          ff[i] = j;
-          break;
-        }
+  printf("Enter no. of blocks: ");
+  scanf("%d", &bno);
+  printf("\nEnter size of each block: ");
+  for (i = 0; i < bno; i++)
+    scanf("%d", &bsize[i]);
+  printf("\nEnter no. of processes: ");
+  scanf("%d", &pno);
+  printf("\nEnter size of each process: ");
+  for (i = 0; i < pno; i++)
+    scanf("%d", &psize[i]);
+
+  for (i = 0; i < pno; i++)
+    for (j = 0; j < bno; j++)
+      if (flags[j] == 0 && bsize[j] >= psize[i]) {
+        allocation[j] = i;
+        flags[j] = 1;
+        break;
       }
-    }
-    frag[i] = temp;
-    bf[ff[i]] = 1;
+  printf("\nBlock no.\tsize\t\tprocess no.\t\tsize");
+  for (i = 0; i < bno; i++) {
+    printf("\n%d\t\t%d\t\t", i + 1, bsize[i]);
+    if (flags[i] == 1)
+      printf("%d\t\t\t%d", allocation[i] + 1, psize[allocation[i]]);
+    else
+      printf("Not allocated");
   }
-  printf("\nFile_no:\tFile_size :\tBlock_no:\tBlock_size:\tFragement");
-  for (i = 1; i <= nf; i++)
-    printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d", i, f[i], ff[i], b[ff[i]], frag[i]);
 }
