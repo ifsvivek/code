3790593069ec76cb9e0958c298dbfe36c833d8a5
ifsvivek <vs63961@gmail.com>
2024-02-19 00:41:06 +0530

    lll

--- Java/random/Triangle.java
+++ Java/random/Triangle.java
@@ -1,28 +1,32 @@
 public class triangle {
     private double side1, side2, side3;
+
     public triangle() {
         side1 = side2 = side3 = 0;
     }
+
     public triangle(double side1, double side2, double side3) {
         this.side1 = side1;
         this.side2 = side2;
         this.side3 = side3;
     }
+
     public double Area() {
         double s = (side1 + side2 + side3) / 2;
         return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
     }
+
     public String Check() {
-        if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
+        if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2)
             return "Not a Triangle";
-        } else if (side1 == side2 && side2 == side3) {
+        else if (side1 == side2 && side2 == side3)
             return "Equilateral Triangle";
-        } else if (side1 == side2 || side2 == side3 || side1 == side3) {
+        else if (side1 == side2 || side2 == side3 || side1 == side3)
             return "Isosceles Triangle";
-        } else {
+        else
             return "Scalene Triangle";
-        }
     }
+
     public static void main(String[] args) {
         triangle triangle1 = new triangle(3, 4, 5);
         System.out.println("Area of triangle1: " + triangle1.Area());
