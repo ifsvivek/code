281f87e2e9595f49e0a277824a6e1eb7926f55a2
ifsvivek <vs63961@gmail.com>
2024-02-22 21:23:55 +0530

    Refactor code to improve performance and readability

---
+++ C/testing/test5.c
@@ -0,0 +1,79 @@
+// tic tac toe game
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define SIZE 3
+
+void printBoard(char board[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            printf("%c ", board[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+bool isFull(char board[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (board[i][j] == ' ') {
+                return false;
+            }
+        }
+    }
+    return true;
+}   
+
+bool checkWin(char board[SIZE][SIZE], char player) {
+    for (int i = 0; i < SIZE; i++) {
+        if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+            return true;
+        }
+    }
+    for (int i = 0; i < SIZE; i++) {
+        if (board[0][i] == player && board[1][i] == player && board[2][i] == player) {
+            return true;
+        }
+    }
+    if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+        return true;
+    }
+    if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    char board[SIZE][SIZE];
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            board[i][j] = ' ';
+        }
+    }
+    printBoard(board);
+    char player = 'X';
+    while (true) {
+        int row, col;
+        printf("Enter row and column: ");
+        scanf("%d %d", &row, &col);
+        if (row < 0 || row >= SIZE || col < 0 || col >= SIZE || board[row][col] != ' ') {
+            printf("Invalid move\n");
+            continue;
+        }
+        board[row][col] = player;
+        printBoard(board);
+        if (checkWin(board, player)) {
+            printf("%c wins!\n", player);
+            break;
+        }
+        if (isFull(board)) {
+            printf("It's a draw!\n");
+            break;
+        }
+        player = (player == 'X') ? 'O' : 'X';
+    }
+    return 0;
+}
