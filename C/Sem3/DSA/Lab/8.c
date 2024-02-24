cd2b78705841746db0003689cf379976599e7795
ifsvivek <vs63961@gmail.com>
2024-02-24 23:19:53 +0530

    cv

--- C/Sem3/DSA/Lab/8.c
+++ C/Sem3/DSA/Lab/8.c
@@ -16,8 +16,7 @@ node* getNode() {
   node* newnode;
   newnode = (node*)malloc(sizeof(node));
   printf("Enter SSN, Name, Department, Designation, Salary, Phone number\n");
-  scanf("%d%s%s%s%d%s", &newnode->ssn, newnode->name, newnode->dept,
-        newnode->desg, &newnode->sal, newnode->phno);
+  scanf("%d%s%s%s%d%s", &newnode->ssn, newnode->name, newnode->dept,newnode->desg, &newnode->sal, newnode->phno);
   newnode->prev = NULL;
   newnode->next = NULL;
   return newnode;
@@ -51,8 +50,7 @@ void deletebegin() {
     printf("List is empty\n");
     return;
   }
-  printf("Deleted node is\n%d\t%s\t%s\t%s\t%d\t%s\n", temp->ssn, temp->name,
-         temp->dept, temp->desg, temp->sal, temp->phno);
+  printf("Deleted node is\n%d\t%s\t%s\t%s\t%d\t%s\n", temp->ssn, temp->name,temp->dept, temp->desg, temp->sal, temp->phno);
   start->prev = NULL;
   start = temp->next;
   free(temp);
@@ -64,8 +62,7 @@ void deleteend() {
     return;
   }
   if (start->next == NULL) {
-    printf("Deleted node is\n%d\t%s\t%s\t%s\t%d\t%s\n", temp->ssn, temp->name,
-           temp->dept, temp->desg, temp->sal, temp->phno);
+    printf("Deleted node is\n%d\t%s\t%s\t%s\t%d\t%s\n", temp->ssn, temp->name,temp->dept, temp->desg, temp->sal, temp->phno);
 
     start = NULL;
     free(temp);
@@ -73,8 +70,7 @@ void deleteend() {
     while (temp->next != NULL)
       temp = temp->next;
     (temp->prev)->next = NULL;
-    printf("Deleted node is\n%d\t%s\t%s\t%s\t%d\t%s\n", temp->ssn, temp->name,
-           temp->dept, temp->desg, temp->sal, temp->phno);
+    printf("Deleted node is\n%d\t%s\t%s\t%s\t%d\t%s\n", temp->ssn, temp->name,temp->dept, temp->desg, temp->sal, temp->phno);
     free(temp);
   }
 }
@@ -88,8 +84,7 @@ void display() {
   }
   printf("List elements are\n");
   while (temp != NULL) {
-    printf("\n%d\t%s\t%s\t%d\t%s\n ", temp->ssn, temp->name, temp->desg,
-           temp->sal, temp->phno);
+    printf("\n%d\t%s\t%s\t%d\t%s\n ", temp->ssn, temp->name, temp->desg,temp->sal, temp->phno);
 
     temp = temp->next;
     count++;
@@ -101,33 +96,20 @@ int main() {
   int choice, n, i;
   while (1) {
     printf(
-        "Enter your choice \n1. InsertEnd\n2. InsertBegin\n3. DeleteBegin\n4. "
-        "DeleteEnd\n5. Display\n6. Exit\n");
+        "Enter your choice \n1. InsertEnd\n2. InsertBegin\n3. DeleteBegin\n4. DeleteEnd\n5. Display\n6. Exit\n");
     scanf("%d", &choice);
     switch (choice) {
       case 1:
         printf("Enter the no of employees\n");
         scanf("%d", &n);
-        for (i = 0; i < n; i++)
-          insertend();
+        for (i = 0; i < n; i++)insertend();
         break;
-      case 2:
-        insertbegin();
-        break;
-      case 3:
-        deletebegin();
-        break;
-      case 4:
-        deleteend();
-        break;
-      case 5:
-        display();
-        break;
-      case 6:
-        exit(0);
-        break;
-      default:
-        printf("Invalid choice\n");
+      case 2:insertbegin();break;
+      case 3:deletebegin();break;
+      case 4:deleteend();break;
+      case 5:display();break;
+      case 6:exit(0);
+      default:printf("Invalid choice\n");
     }
   }
 }
