42329a8182a9584361a4299863fc3468a38c3b7d
ifsvivek <vs63961@gmail.com>
2024-02-27 10:20:37 +0530

    Refactor jkff module and update test files

--- other/DDCO/jkff.v
+++ other/DDCO/jkff.v
@@ -1,10 +1,11 @@
-module jkff(q, qbar, clk, j,k);
-input j, k, clk;
-output q, qbar;
-wire nand1_out;
-wire nand2_out;
-nand (nand1_out, j, clk, qbar);
-nand (nand2_out, k, clk, q);
- nand (q, qbar, nand1_out);
- nand (qbar, q, nand2_out);
+module jkff(q, qbar, clk, j, k);
+    input j, k, clk;
+    output q, qbar;
+    wire nand1_out;
+    wire nand2_out;
+    
+    nand (nand1_out, j, clk, qbar);
+    nand (nand2_out, k, clk, q);
+    nand (q, qbar, nand1_out);
+    nand (qbar, q, nand2_out);
 endmodule
