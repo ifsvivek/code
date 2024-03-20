b3265090e4c1d1cb547d6c268adbf6c99d3458c6
ifsvivek <vs63961@gmail.com>
2024-03-20 19:17:38 +0530

    Refactored sum calculation logic in sum.c

--- C/testing/sum.c
+++ C/testing/sum.c
@@ -1,8 +1,22 @@
+// code to find sum of digits in a number
+// but if sum of digits is greater than 9 then again find sum of digits of that number
+// eg: 5883 => 5+8=13 => 1+3=4 4+8=12 => 1+2=3 3+3=6 => 6
+
 #include <stdio.h>
+
 int main() {
-    int a, b, c, d;
-    printf("enter 3 digit number ");
-    scanf("%1d%1d%1d", &a, &b, &c);
-    d = a + b + c;
-    printf("%d", d);
+    int n, sum = 0, rem;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    while (n > 0) {
+        rem = n % 10;
+        sum += rem;
+        n = n / 10;
+        if (n == 0 && sum > 9) {
+            n = sum;
+            sum = 0;
+        }
+    }
+    printf("Sum of digits is: %d", sum);
+    return 0;
 }
