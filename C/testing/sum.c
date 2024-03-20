06122ea017376a796c071c5064a14b0ad0849f7b
ifsvivek <vs63961@gmail.com>
2024-03-20 19:20:10 +0530

    Refactor sum of digits calculation in sum.c

--- C/testing/sum.c
+++ C/testing/sum.c
@@ -1,6 +1,8 @@
-// code to find sum of digits in a number
-// but if sum of digits is greater than 9 then again find sum of digits of that number
-// eg: 5883 => 5+8=13 => 1+3=4 4+8=12 => 1+2=3 3+3=6 => 6
+// Code to find the sum of digits in a number.
+// If the sum of digits is greater than 9, the sum of the digits of the sum is calculated.
+// This process is repeated until the sum is less than 10.
+// For example, for the number 5883, the sum of the digits is calculated as follows:
+// 5 + 8 + 8 + 3 = 24 => 2 + 4 = 6
 
 #include <stdio.h>
 
@@ -8,14 +10,14 @@ int main() {
     int n, sum = 0, rem;
     printf("Enter a number: ");
     scanf("%d", &n);
-    while (n > 0) {
-        rem = n % 10;
-        sum += rem;
-        n = n / 10;
-        if (n == 0 && sum > 9) {
+    while (n > 0 || sum > 9) {
+        if (n == 0) {
             n = sum;
             sum = 0;
         }
+        rem = n % 10;
+        sum += rem;
+        n = n / 10;
     }
     printf("Sum of digits is: %d", sum);
     return 0;
