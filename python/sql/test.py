a206db276b88e7db87e5abf25f750667442beb87
ifsvivek <vs63961@gmail.com>
2024-05-06 21:05:20 +0530

    chore: Update MySQL connection code to use consistent quotation marks and formatting

--- python/sql/test.py
+++ python/sql/test.py
@@ -2,9 +2,22 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
     host="localhost",
-    database='test01',
+    database="test01",
     user="root",
     password="1234",
-    auth_plugin='mysql_native_password'
+    auth_plugin="mysql_native_password",
 )
 
+
+# code to create a table with the name "customers" and the columns "name" and "address":
+
+mycursor = mydb.cursor()
+
+# mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+
+# code to check if a table exists, and if not, create it:
+
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+    print(x)
